878352435c916e74eea5846ef13f51ce59d2b7cd
Rost Myshkin <rostmyshkin@gmail.com>
2020-05-22 20:27:38 -0700

    content; checkbox; tasks

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { SideBar } from "./SideBar";
+import { Tasks } from "../Tasks";
 
 export const Content = () => (
   <section>
     <SideBar />
+    <Tasks />
   </section>
 );
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
 import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyA4O7dPkGlxQ7--eq84b0TTolNP6xLnBV0",
-  authDomain: "todoist-clone-91831.firebaseapp.com",
-  databaseURL: "https://todoist-clone-91831.firebaseio.com",
-  projectId: "todoist-clone-91831",
-  storageBucket: "todoist-clone-91831.appspot.com",
-  messagingSenderId: "212688760177",
-  appId: "1:212688760177:web:0d47001aad025c97911ad0",
+  apiKey: process.env.REACT_APP_TODOIST_APIKEY,
+  authDomain: process.env.REACT_APP_TODOIST_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_TODOIST_DBURL,
+  projectId: process.env.REACT_APP_TODOIST_PROJECTID,
+  storageBucket: process.env.REACT_APP_TODOIST_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_TODOIST_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_TODOIST_APPID,
 });
 
 export { firebaseConfig as firebase };
